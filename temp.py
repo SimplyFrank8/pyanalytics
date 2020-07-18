77c5f9fbca020307f1b609475813a46b94f0f8a7
SimplyFrank8 <68459398+SimplyFrank8@users.noreply.github.com>
2020-07-18 19:03:56 +0530

    18-07-2020, Samarth Ladia

---
+++ temp.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+list1=[1,2,3] #F9 to exeute the line, Ctrl enter for selected, shift enter for selection and going to the next line
+list1
+print(list1)
